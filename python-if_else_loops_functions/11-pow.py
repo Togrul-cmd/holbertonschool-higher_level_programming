480af59e94fe01c039421ccd53db42eb14117e29
Togrul-cmd <togucours@gmail.com>
2026-06-11 13:18:39 +0000

    a^b just operator

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    pr = 1
-    if b < 0:
-        while b:
-            pr /= a
-            b += 1
-    else:
-        while b:
-            pr *= a
-            b -= 1
-    return pr
+    return a ** b
