3f1c74d49a33fbb22475365a6b8ea4b1a54dd27c
Togrul-cmd <togucours@gmail.com>
2026-06-11 12:27:53 +0000

    Changing str to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if 97 <= ord(i) <= 123:
+            print('{}'.format(chr(ord(i)-32)), end='')
+        else:
+            print('{}'.format(i), end='')
+    print()
