cb69693aa62d2dec3cecd4dd7bb88b190c596604
Togrul-cmd <togucours@gmail.com>
2026-06-30 16:04:35 +0000

    Another fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     from sys import argv, exit
-    count = len(argv) - 1
-    op = ['+', '-', '*', '/']
-    if count != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif argv[2] not in op:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        a = int(argv[1])
-        b = int(argv[3])
-        match argv[2]:
-            case '+':
-                print('{} + {} = {}'.format(a, b, add(a, b)))
-            case '-':
-                print('{} - {} = {}'.format(a, b, sub(a, b)))
-            case '*':
-                print('{} * {} = {}'.format(a, b, mul(a, b)))
-            case '/':
-                print('{} / {} = {}'.format(a, b, div(a, b)))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
