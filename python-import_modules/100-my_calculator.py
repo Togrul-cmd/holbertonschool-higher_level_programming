170ce007a4ab3c678cb1501a1f703c040a846576
Togrul-cmd <togucours@gmail.com>
2026-06-30 15:59:16 +0000

    indent

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,20 +5,20 @@ if __name__ == "__main__":
     count = len(argv) - 1
     op = ['+', '-', '*', '/']
     if count != 3:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
     elif argv[2] not in op:
-    print('Unknown operator. Available operators: +, -, * and /')
-    exit(1)
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
     else:
-    a = int(argv[1])
-    b = int(argv[3])
-    match argv[2]:
-        case '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-        case '-':
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-        case '*':
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-        case '/':
-        print('{} / {} = {}'.format(a, b, div(a, b)))
+        a = int(argv[1])
+        b = int(argv[3])
+        match argv[2]:
+            case '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+            case '-':
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+            case '*':
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+            case '/':
+            print('{} / {} = {}'.format(a, b, div(a, b)))
