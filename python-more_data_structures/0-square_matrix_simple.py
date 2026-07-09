ab812adafe47b8fa16cf7865bdf2f27e8964b80f
Togrul-cmd <togucours@gmail.com>
2026-07-09 09:45:58 +0000

    new soln

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[j**2 for j in i] for i in matrix]
+    l=list(map(lambda x : list(map(lambda y: y**2, x)), matrix))
+    return l
