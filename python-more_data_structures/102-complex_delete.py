0ed2587aca4fec97171a78b5d8dee42130f2a837
Togrul-cmd <togucours@gmail.com>
2026-07-10 12:40:04 +0000

    micro-optimization

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    key_list = [key for key in a_dictionary if a_dictionary[key] == value]
+    key_list = [key for key, val in a_dictionary.items() if val == value]
     for i in key_list:
         del a_dictionary[i]
     return a_dictionary
