7d355187e20c5ab414b3f84fb621ec7193fdfc3a
Togrul-cmd <togucours@gmail.com>
2026-07-10 08:45:30 +0000

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    romans = {'I' : 1, 'IV' : 4, 'V' : 5, 'IX' : 9, 'X' : 10, 'XL' : 40, 'L' : 50, 'XC' : 90, 'C' : 100, 'CD' : 400, 'D' : 500, 'CM' : 900, 'M' : 1000}
+    romans = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
+    'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
     index = 0
     num = 0
     while index < len(roman_string):
@@ -11,9 +12,8 @@ def roman_to_int(roman_string):
             index += 1
             if index < len(roman_string):
                 s += roman_string[index]
-        if len(s) > 1 and not s in romans:    
+        if len(s) > 1 and s not in romans:  
             num += romans[s[:-1]]
         else:
             num += romans[s]
     return num
-        
