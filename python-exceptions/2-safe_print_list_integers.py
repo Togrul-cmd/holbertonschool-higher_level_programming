e12e7e02ec1beb5600347e88772c6ed10d3231e3
Togrul-cmd <togucours@gmail.com>
2026-07-11 22:29:09 +0000

    mistype

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if not isinstance(my_list[x], int):
+            if not isinstance(my_list[i], int):
                 continue
             print('{:d}'.format(my_list[i]), end='')
             count += 1
