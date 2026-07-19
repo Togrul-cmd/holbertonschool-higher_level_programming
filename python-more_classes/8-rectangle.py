73265cf06aedf52ad1ccf16450e17b3ac38e6f58
Togrul-cmd <togucours@gmail.com>
2026-07-19 13:49:09 +0000

    Prints rectangle with print symbol

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""This module defines a class Rectangle."""
+
+
+class Rectangle:
+    """Defines rectangle object wiht width and height attributes."""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """Instantiates with optional width and height."""
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """Prints the rectangle with print_symbol attribute."""
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        return '\n'.join([str(self.print_symbol) * self.__width] *
+                         self.__height)
+
+    def __repr__(self):
+        """Returns internal representation of the object."""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Deletes object."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area."""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @property
+    def width(self):
+        """Retrieves width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Checks value type and sets width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Checks value and sets height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and returns area of rectangle."""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculates and returns perimeter of rectangle."""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
