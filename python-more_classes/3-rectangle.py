ce33122f200978895b0b3b7489d9b878f15d12f6
Togrul-cmd <togucours@gmail.com>
2026-07-19 11:50:28 +0000

    With printable string representation

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module defines a class Rectangle."""
+
+
+class Rectangle:
+    """Defines rectangle object wiht width and height attributes."""
+    def __init__(self, width=0, height=0):
+        """Instantiates with optional width and height."""
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """Print the rectangle with the character #."""
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        return '\n'.join(['#' * self.__width] * self.__height)
+
+    @property
+    def width(self):
+        """Retrieves width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Checks value type and sets width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Checks value and sets height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and returns area of rectangle."""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculates and returns perimeter of rectangle."""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
