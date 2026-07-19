582e75ebb0d67e0de2e6cbf4b19663ddb844e9d9
Togrul-cmd <togucours@gmail.com>
2026-07-19 11:07:37 +0000

    An empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines an empty class Rectangle."""
+
+
+class Rectangle:
+    """It is an empty class."""
+    def __init__(self):
+        """Symbolic instantiation method."""
+        pass
