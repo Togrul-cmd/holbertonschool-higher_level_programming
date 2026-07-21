2467f0701785382d4f3acc89a91395cbb9122260
Togrul-cmd <togucours@gmail.com>
2026-07-21 19:05:52 +0000

    A class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This module defines an empty class.
+"""
+
+
+class BaseGeometry:
+    """This is an empty class."""
+    def area(self):
+        """Raises exception."""
+        raise Exception("area() is not implemented")
