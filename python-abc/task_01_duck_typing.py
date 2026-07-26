fdc025e28d007aa0c69ad233ad2b53a80d997df4
Togrul-cmd <togucours@gmail.com>
2026-07-26 04:56:03 +0000

    no docstring

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,60 +1,44 @@
-"""
-Module defining an abstract Shape class and two subclasses.
-"""
-
 from abc import ABC, abstractmethod
 from math import pi
 
 
 class Shape(ABC):
-    """Abstract Shape class."""
 
     @abstractmethod
     def area(self):
-        """Calculates area."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Calculates perimeter."""
         pass
 
 
 class Circle(Shape):
-    """Circle class inheriting from Shape."""
 
     def __init__(self, radius):
-        """Initializes a Circle."""
         self.radius = radius
 
     def area(self):
-        """Calculates the area of the circle."""
         return pi * self.radius ** 2
 
     def perimeter(self):
-        """Calculates the perimeter of the circle."""
         return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
-    """Rectangle class inheriting from Shape."""
 
     def __init__(self, width, height):
-        """Initializes a Rectangle."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Calculates the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Calculates the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Prints the area and perimeter of a shape."""
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
 
