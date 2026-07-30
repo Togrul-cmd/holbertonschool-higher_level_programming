1ad58b7ed7af16742f1e3ce69beaefb111653917
Togrul-cmd <togucours@gmail.com>
2026-07-30 12:08:47 +0000

    docs

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""
+    This module defines a function that
+    calculates the integral of a polynomial.
+"""
+
+
 def poly_integral(poly, C=0):
+    """Calculates the integral of a polynomial."""
     if not isinstance(poly, list) or len(poly) == 0 or type(C) is not int:
         return None
     for coef in poly:
