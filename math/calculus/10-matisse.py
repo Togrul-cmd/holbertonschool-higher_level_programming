73955dd14f472db3df809731407a5a09f3299540
Togrul-cmd <togucours@gmail.com>
2026-07-30 10:30:44 +0000

    Derive happiness in oneself from a good day's work

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,11 +9,11 @@ def poly_derivative(poly):
     """calculates and returns the derivative of a polynomial."""
     if not isinstance(poly, list) or len(poly) == 0:
         return None
-    
+
     for coef in poly:
         if type(coef) not in (int, float):
             return None
-    
+
     if len(poly) == 1:
         return [0]
 
