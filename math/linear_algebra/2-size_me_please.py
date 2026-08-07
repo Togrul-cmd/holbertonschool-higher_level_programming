e635800fd8e4996dfa7bfe23b85780920077281e
Togrul-cmd <togucours@gmail.com>
2026-08-07 14:04:40 +0000

    docs

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+    This module defines a function
+    that calculates the shape of a matrix.
+"""
+
+
 def matrix_shape(matrix):
     """A function that calculates the shape of a matrix."""
     shape = []
