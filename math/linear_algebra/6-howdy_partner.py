47b41e4619282c1320a21af65cc8923e3a5ba0f9
Togrul-cmd <togucours@gmail.com>
2026-08-09 13:14:12 +0000

    Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    This module defines a function that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays."""
+    return arr1 + arr2
