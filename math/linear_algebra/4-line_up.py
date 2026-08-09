b4c26850142a9bb72c26a43fb35efc41e13a32a7
Togrul-cmd <togucours@gmail.com>
2026-08-09 12:42:55 +0000

    Line Up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    This module defines a func that adds two arrays element-wise.
+"""
+def add_arrays(arr1, arr2):
+    """Adds two arrays element-wise."""
+    if len(arr1) != len(arr2):
+        return None
+    return [a + b for a, b in zip(arr1, arr2)]
