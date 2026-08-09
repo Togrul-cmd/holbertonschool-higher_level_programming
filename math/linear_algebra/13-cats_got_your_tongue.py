4a4717b5e9f6121de126a5be0cc68b9de1eb3292
Togrul-cmd <togucours@gmail.com>
2026-08-09 21:56:37 +0000

    Cat's Got Your Tongue

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to concatenate two matrices along a specific axis."""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis."""
+    return np.concat((mat1, mat2), axis=axis)
