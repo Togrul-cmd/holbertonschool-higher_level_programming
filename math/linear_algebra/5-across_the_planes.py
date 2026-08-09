75b9b0600debb7e336fe6f09b704a1e50d0ba26d
Togrul-cmd <togucours@gmail.com>
2026-08-09 13:08:50 +0000

    Across The Planes

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module to add two 2D matrices element-wise."""
+
+
+def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise."""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[a + b for a, b in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
