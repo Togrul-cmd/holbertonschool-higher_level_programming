a56964fc1359eafec521feceed3de5aa26dcf4ea
Togrul-cmd <togucours@gmail.com>
2026-08-10 13:14:08 +0000

    changes

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -2,17 +2,17 @@
 """Module to add two matrixes."""
 
 
+def formatting(lst):
+    if isinstance(lst, list) and None in lst:
+        return None
+    return lst
+
+
 def add_matrices(mat1, mat2):
-    """Adds 2 matrixes."""
     if not isinstance(mat1, list) and not isinstance(mat2, list):
         return mat1 + mat2
     if not isinstance(mat1, list) or not isinstance(mat2, list)
     or len(mat1) != len(mat2):
         return None
-    result = []
-    for add1, add2 in zip(mat1, mat2):
-        sub_result = add_matrices(add1, add2)
-        if sub_result is None:
-            return None
-        result.append(sub_result)
-    return result
+    return formatting([add_matrices(add1, add2) for
+                       add1, add2 in zip(mat1, mat2)])
