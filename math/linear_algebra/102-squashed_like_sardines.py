1fbfe37152cf53a91d77a2c487ff64d4d348e0d3
Togrul-cmd <togucours@gmail.com>
2026-08-17 11:10:42 +0000

    again

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -2,23 +2,47 @@
 """Module to concatenate two matrices along a specific axis."""
 
 
+def shape(matrix):
+    """Calculates the shape of an N-dimensional matrix."""
+    matrix_shape = []
+    curr = matrix
+    while isinstance(curr, list):
+        matrix_shape.append(len(curr))
+        if len(curr) == 0:
+            break
+        curr = curr[0]
+    return tuple(matrix_shape)
+
+
 def cat_matrices(mat1, mat2, axis=0):
-    """Concatenate two matrices along a specific axis."""
+    """Concatenates two N-dimensional matrices along a given axis."""
     if not isinstance(mat1, list) or not isinstance(mat2, list):
         return None
-    if axis == 0:
-        if isinstance(mat1[0], list) and isinstance(mat2[0], list):
-            if cat_matrices(mat1[0], mat2[0], axis=0) is None:
-                return None
-        elif isinstance(mat1[0], list) != isinstance(mat2[0], list):
-            return None
-        return mat1 + mat2
-    if len(mat1) != len(mat2):
+
+    s1 = shape(mat1)
+    s2 = shape(mat2)
+
+    # 1. Both matrices must have the same number of dimensions
+    if len(s1) != len(s2):
         return None
-    result = []
-    for cat1, cat2 in zip(mat1, mat2):
-        sub_result = cat_matrices(cat1, cat2, axis - 1)
-        if sub_result is None:
+
+    # 2. Axis must be within bounds
+    if axis < 0 or axis >= len(s1):
+        return None
+
+    # 3. All dimensions EXCEPT `axis` must match exactly
+    for i in range(len(s1)):
+        if i != axis and s1[i] != s2[i]:
             return None
-        result.append(sub_result)
-    return result
+
+    # Recursive helper to perform concatenation
+    def concat_recursive(m1, m2, curr_axis):
+        if curr_axis == 0:
+            return m1 + m2
+
+        result = []
+        for sub1, sub2 in zip(m1, m2):
+            result.append(concat_recursive(sub1, sub2, curr_axis - 1))
+        return result
+
+    return concat_recursive(mat1, mat2, axis)
