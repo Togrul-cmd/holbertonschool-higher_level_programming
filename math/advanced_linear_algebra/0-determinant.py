484a89f8e119349a0852196781f86cac676acd58
Togrul-cmd <togucours@gmail.com>
2026-08-24 11:00:43 +0000

    mistype

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -39,7 +39,7 @@ def determinant(matrix):
     mat_l = len(matrix)
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if not all([isinstance(matrix, list) for mat in matrix]):
+    if not all([isinstance(mat, list) for mat in matrix]):
         raise TypeError("matrix must be a list of lists")
     if matrix[0] and mat_l != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
