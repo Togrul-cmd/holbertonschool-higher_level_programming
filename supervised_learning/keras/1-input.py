4c25a9f295fe6a1cd3941cb6dcc68a855dd817b6
Togrul-cmd <togucours@gmail.com>
2026-08-27 09:35:05 +0000

    dropped weights

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -16,18 +16,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
         not allowed to use Sequential class
     """
-    weights = K.initializers.VarianceScaling(mode="fan_avg")
     reg = K.regularizers.l2(lambtha)
     inputs = K.Input(shape=(nx,))
     layer = K.layers.Dense(layers[0],
                            activation=activations[0],
-                           kernel_initializer=weights,
                            kernel_regularizer=reg)(inputs)
     for i in range(1, len(layers)):
         drop = K.layers.Dropout(rate=(1 - keep_prob))(layer)
         reg = K.regularizers.l2(lambtha)
         layer = K.layers.Dense(layers[i],
                                activation=activations[i],
-                               kernel_initializer=weights,
                                kernel_regularizer=reg)(drop)
     return K.Model(inputs=inputs, outputs=layer)
