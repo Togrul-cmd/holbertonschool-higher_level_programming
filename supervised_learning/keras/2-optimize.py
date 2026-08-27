a516e30f4453e1ab243485cdbc71bdf5105734ff
Togrul-cmd <togucours@gmail.com>
2026-08-27 09:23:10 +0000

    learning rate

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -22,7 +22,7 @@ def optimize_model(network, alpha, beta1, beta2):
     returns:
         None
     """
-    network.compile(optimizer=K.optimizers.Adam(lr=alpha,
+    network.compile(optimizer=K.optimizers.Adam(learning_rate=alpha,
                                                 beta_1=beta1,
                                                 beta_2=beta2),
                     loss='categorical_crossentropy',
