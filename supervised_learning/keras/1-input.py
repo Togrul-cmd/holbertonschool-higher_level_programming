7b30711476eb94e1d20d6f6a037b7076bf883886
Togrul-cmd <togucours@gmail.com>
2026-08-27 09:45:19 +0000

    He Normal initialization

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
 """
-    Keras project (finally)
+Builds a neural network with Keras
 """
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """ Buils a NN with Keras
-        nx is number of input features
-        layers is list containing the # of nodes in each layer
-        activations is list containing activations used for each layer
-        labmtha is L2 regularization parameter
-        keep_prob is probability that node will be kept
-        Returns: keras model
+    """
+    Builds a neural network with Keras library
 
-        not allowed to use Sequential class
+    parameters:
+        nx [int]: number of input features
+        layers [list]: number of nodes in each layer
+        activations [list]: activation function for each layer
+        lambtha [float]: L2 regularization parameter
+        keep_prob [float]: probability that a node will be kept for dropout
+
+    returns:
+        the compiled keras model
     """
     reg = K.regularizers.l2(lambtha)
+    init = K.initializers.he_normal()
     inputs = K.Input(shape=(nx,))
-    layer = K.layers.Dense(layers[0],
-                           activation=activations[0],
-                           kernel_regularizer=reg)(inputs)
+
+    # First layer
+    layer = K.layers.Dense(
+        layers[0],
+        activation=activations[0],
+        kernel_initializer=init,
+        kernel_regularizer=reg
+    )(inputs)
+
+    # Remaining layers
     for i in range(1, len(layers)):
         drop = K.layers.Dropout(rate=(1 - keep_prob))(layer)
-        reg = K.regularizers.l2(lambtha)
-        layer = K.layers.Dense(layers[i],
-                               activation=activations[i],
-                               kernel_regularizer=reg)(drop)
+        layer = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_initializer=init,
+            kernel_regularizer=reg
+        )(drop)
+
     return K.Model(inputs=inputs, outputs=layer)
